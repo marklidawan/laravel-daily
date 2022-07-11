ecbeca00f879cac5cd1a10c11e53a6e3e3ca68a0
unknown <marklidawan@gmail.com>
2022-07-11 14:21:24 +0800

    main menu

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::view('/', view: 'index')->name(name: 'home');
+Route::view('/about', view: 'about')->name(name: 'about');
+Route::view('/contact', view: 'contact')->name(name: 'contact');
 
